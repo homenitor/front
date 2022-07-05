e8a528372f24c69004e74ee17bb7e81b9f6bf929
Kroustille <6004961+Kroustille@users.noreply.github.com>
2022-07-05 21:30:41 +0200

    tech: add storybook with first component

---
+++ src/ui/probe/TemperatureGraph.stories.tsx
@@ -0,0 +1,12 @@
+import { TemperatureGraph } from './TemperatureGraph'
+
+export default {
+  title: 'TemperatureGraph',
+  component: TemperatureGraph
+}
+
+const Template = (args: any) => <TemperatureGraph {...args} />
+
+export const FirstStory = {
+  args: {}
+}
---
+++ src/ui/probe/TemperatureGraph.tsx
@@ -0,0 +1,3 @@
+export const TemperatureGraph = () => {
+  return <div>Hello</div>
+}
